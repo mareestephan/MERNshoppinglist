60c8e54ca813cc8229f19053ce77514b8a12e120
mareestephan <stephanmaree@gmail.com>
2018-11-23 19:36:14 +0200

    reactstrap

---
+++ client/src/components/Appnavbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    NavBar,
+    NavBarToggler,
+    NavBarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from ' reactstrap';
+
+class AppNavBarr extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+}
+
+export default APpNavBar;
